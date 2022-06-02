266fd0eb6cdf4caaca7404385785e9a08bd4637e
A S H K A N <ashkan.alavinia@gmail.com>
2022-06-01 21:34:11 -0400

    init

--- NumbersCombinations.go
+++ NumbersCombinations.go
@@ -1,1 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"gonum.org/v1/gonum/stat/combin"
+)
+
+func main() {
+	data := []int{1, 2, 3, 4, 5, 6}
+	cs := combin.Combinations(len(data), 3)
+	for _, c := range cs {
+		fmt.Printf("%d%d%d\n", data[c[0]], data[c[1]], data[c[2]])
+	}
+}
 
